2fd179f7669cca71af4ab3c5e40095f17f2a4a49
TorinM321 <110474253+TorinM321@users.noreply.github.com>
2022-08-08 19:57:51 +0100

    Rearanging the GUI to look better

--- main.py
+++ main.py
@@ -33,22 +33,25 @@ if __name__ == '__main__':
     root.title("Get Medical Files")
     root.resizable(False, False)
     root.geometry("1500x1000")
+    root.configure(bg="#DFFEFF")
     windll.shcore.SetProcessDpiAwareness(1)
+
+    root.tk.call('tk', 'scaling', 2.5)
+
 
     current_date = date.today()
 
+    dt1 = date(2010,12,12)
     cal = Calendar(root, selectmode='day',
-                   year=current_date.year, month=current_date.month, day=current_date.day, date_pattern="yyyy/m/d")
+                   year=current_date.year, month=current_date.month, day=current_date.day, date_pattern="yyyy/m/d",mindate=dt1)
 
-    Button(root, text="Get Date", command=getDates).place(x=700, y=300)
-
-    #https://stackoverflow.com/questions/72670026/calendar-size-too-small-in-tkcalendar
+    Button(root, text="Get Files", command=getDates).place(x=525, y=210)
 
     dates = Label(root, text="")
-    showTxt = Label(root, text="Please select a date for the files you want to see: ", font=("Helvetica bold", 18))
+    showTxt = Label(root, text="Please select a date for the files you want to see: ", font=("Helvetica bold", 10), bg="#DFFEFF")
 
-    showTxt.place(x=50, y=20)
-    cal.place(x=50, y=80)
+    showTxt.place(x=35, y=35)
+    cal.place(x=50, y=100)
     dates.place(x=600, y=400)
 
     root.mainloop()
