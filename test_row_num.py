3cb3aac4640d60a9eabbbb66b5176841b4c45b3c
EthSpJo <bluespjem@gmail.com>
2022-08-12 14:41:34 +0100

    check_num_rows tests
    All pass

--- test_row_num.py
+++ test_row_num.py
@@ -1,10 +1,21 @@
 import unittest
+import pandas as pd
 
-from validate_file import ccheck_num_rows
+from os.path import exists
+from validate_file import check_num_rows
 
 class RowNumUnitTest(unittest.TestCase):
-      ""Tests for the validate_file module""
+      #Tests for the validate_file module""
+      def test_cnr1(self):
+          # False should be returned as the file being checked has the desired number of rows
+          self.assertFalse(check_num_rows(pd.read_csv('./tests/testDoc1Valid.csv'),"testDoc1Valid.csv"))
+          #Passes 12/08/2022
 
+      def test_cnr2(self):
+          # A log file containing error messages, etc should be produced as test file passed in has fewer rows then required.
+          check_num_rows(pd.read_csv('./tests/testDoc3InvalidRC.csv'),"testDoc3InvalidRC.csv")
+          self.assertTrue(exists('testDoc3InvalidRC_log.txt'))
+          # Passed 12/08/2022
 
-if _name_ == 'main':
-   unittest.main(verebosity=2)
+if __name__ == '__main__':
+   unittest.main()
