c896be95013e8595f2fe98a719a45d987a3cb50f
EthSpJo <bluespjem@gmail.com>
2022-08-12 15:10:26 +0100

    check_num_columns tests
    All Pass

--- test_num_columns.py
+++ test_num_columns.py
@@ -1,10 +1,21 @@
 import unittest
+import pandas as pd
 
+from os.path import exists
 from validate_file import check_num_columns
 
 class ColNumUnitTest(unittest.TestCase):
-      ""Tests for the validate_file module""
+      #Tests for the validate_file module
+      def test_cnc1(self):
+          # False should be returned as the file being checked has the desired number of columns
+          self.assertFalse(check_num_columns(pd.read_csv('./tests/testDoc1Valid.csv'),"testDoc1Valid.csv"))
+          #Passes 12/08/2022
 
+      def test_cnc2(self):
+          # A log file containing error messages, etc should be produced as test file passed in has unwanted additional comlumns
+          check_num_columns(pd.read_csv('./tests/testDoc2.1Invalid.csv'),"testDoc2.1Invalid.csv")
+          self.assertTrue(exists('testDoc2.1Invalid_log.txt'))
+          #Passes 12/08/2022
 
-if _name_ == 'main':
-   unittest.main(verebosity=2)
+if __name__ == '__main__':
+   unittest.main()
