f39d25cca93685cb7303e399acdfb58d8c7013e1
EthSpJo <bluespjem@gmail.com>
2022-08-13 15:32:59 +0100

    fixed check_id tests
    all pass, return type value check logic added

--- test_check_ids.py
+++ test_check_ids.py
@@ -12,22 +12,30 @@ class IdUnitTest(unittest.TestCase):
            # Passed 12/08/2022
 
       def test_cid2(self):
-          # A log file should be created as test file used has duplicate batch ID's
-          check_ids(pd.read_csv('./tests/testDoc4DupBID.csv'),"testDoc4DupBID.csv")
+          #True will be returned as an invalid batch id is present in the test file
+          self.assertTrue(check_ids(pd.read_csv('./tests/testDoc4DupBID.csv'),"testDoc4DupBID.csv"))
+
+          # A log file should be created as test file used has duplicate batch ID's          
           self.assertTrue(exists('testDoc4DupBID_log.txt'))
            # Passed 12/08/2022
-
+           # Passed 13/08/2022 - checking returned value logic added
 
       def test_cid3(self):
+          #True will be returned as an invalid batch id is present in the test file
+          self.assertTrue(check_ids(pd.read_csv('./tests/testDoc4NegBID.csv'),"testDoc4NegBID.csv"))        
           # A log file, etc should be created as test file used contains a negative batch ID's
-          check_ids(pd.read_csv('./tests/testDoc4NegBID.csv'),"testDoc4NegBID.csv")
-          self.assertTrue(exists('testDocNegBID_log.txt'))
+          self.assertTrue(exists('testDoc4NegBID_log.txt'))
            # Passed 12/08/2022
+           # Passed 13/08/2022 - checking returned value logic added
+
       def test_cid4(self):
+          #True will be returned as an invalid batch id is present in the test file
+          self.assertTrue(check_ids(pd.read_csv('./tests/testDoc4StrBID.csv'),"testDoc4StrBID.csv"))
+         
           # A log file, etc should be created as test file used contains a batch ID of type string
-          check_ids(pd.read_csv('./tests/testDoc4StrBID.csv'),"testDoc4StrBID.csv")
           self.assertTrue(exists('testDoc4StrBID_log.txt'))
            # Passed 12/08/2022
+           # Passed 13/08/2022 - checking returned value logic added
 
 if __name__ == '__main__':
    unittest.main()
