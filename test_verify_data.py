80994b4ee20a06528f8bf178e2a5c44c442f3da5
EthSpJo <bluespjem@gmail.com>
2022-08-13 17:51:02 +0100

    veryify data test document

---
+++ test_verify_data.py
@@ -0,0 +1,10 @@
+import unittest
+
+from validate_file import verify_data
+
+class verifDataUnitTest(unittest.TestCase):
+      #Tests for the validate_file module
+
+
+if __name__ == '__main__':
+   unittest.main()
